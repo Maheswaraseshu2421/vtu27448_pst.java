81d07c2af76fac058d64f4fba38b578d65be624b
Maheswaraseshu2421 <padalaseshu276@gmail.com>
2026-08-15 21:51:47 +0530

    comparator java

---
+++ WEEK1/Session 3/Java comparator.java
@@ -0,0 +1,14 @@
+class Checker implements java.util.Comparator<Player> {
+
+    @Override
+    public int compare(Player a, Player b) {
+
+       
+        if (a.score != b.score) {
+            return b.score - a.score;
+        }
+
+        
+        return a.name.compareTo(b.name);
+    }
+}
